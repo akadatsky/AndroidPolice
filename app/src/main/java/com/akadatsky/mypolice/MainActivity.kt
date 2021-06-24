6f02c5eb6bf77473e58c0c6bb440af07e54837b2
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2021-06-24 21:17:51 +0300

    Add some code

--- app/src/main/java/com/akadatsky/mypolice/MainActivity.kt
+++ app/src/main/java/com/akadatsky/mypolice/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.akadatsky.mypolice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val calendarView = findViewById<View>(R.id.calendarView)
+        calendarView.setOnClickListener {
+            startActivity(Intent(this, CalendarActivity::class.java))
+        }
+
     }
 }
